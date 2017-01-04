0919de45df586e10d9186268c2e98736b566b048
NexusPenguin <fredbottaro.alva@gmail.com>
2017-01-04 22:08:38 +0100

    Update tesla.mk

--- tesla.mk
+++ tesla.mk
@@ -3,6 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
+$(call inherit-product, vendor/tesla/config/caf_fw.mk)
 
 # Inherit some common Tesla stuff.
 $(call inherit-product, vendor/tesla/config/common_full_phone.mk)
